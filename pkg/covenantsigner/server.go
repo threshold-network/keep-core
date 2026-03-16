c3bc5006803b2590a9bec82838bf55279574ccb5
Piotr Roslaniec <p.roslaniec@gmail.com>
2026-03-16 17:48:07 +0000

    fix(covenantsigner): set explicit max header size

--- pkg/covenantsigner/server.go
+++ pkg/covenantsigner/server.go
@@ -108,6 +108,7 @@ func Initialize(
 			ReadTimeout:       30 * time.Second,
 			WriteTimeout:      30 * time.Second,
 			IdleTimeout:       60 * time.Second,
+			MaxHeaderBytes:    1 << 13,
 		},
 	}
 
